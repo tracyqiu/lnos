3ea902e771c1a88089b78bb82bb00af040cf1efb
Qiu <tracyqiu@126.com>
2024-07-29 13:46:28 +0800

    read disk

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ SRC_DIR:=src
 BUILD_DIR:=build
 HD_IMG_NAME:="lnos.img"
 
-all: ${BUILD_DIR}/boot.o
+all: ${BUILD_DIR}/boot.o ${BUILD_DIR}/setup.o
 	$(shell rm -rf $(HD_IMG_NAME))
 	bximage -q -hd=16 -mode=create -sectsize=512 -imgmode=flat $(HD_IMG_NAME)
 	dd if=${BUILD_DIR}/boot.o of=$(HD_IMG_NAME) bs=512 seek=0 count=1 conv=notrunc
-#	dd if=${BUILD_DIR}/boot/setup.o of=$(HD_IMG_NAME) bs=512 seek=1 count=2 conv=notrunc
+	dd if=${BUILD_DIR}/setup.o of=$(HD_IMG_NAME) bs=512 seek=1 count=2 conv=notrunc
 
 ${BUILD_DIR}/%.o: $(SRC_DIR)/%.asm
 	$(shell mkdir -p ${BUILD_DIR})
--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,6 @@
 org 0x7C00
 bits 16
 
-%define ENDL 0x0A, 0x0D
-
 global _start
 _start:
    mov ax, 2
@@ -17,41 +15,29 @@ _start:
    mov ss, ax
    mov sp, 0x7C00
 
-   ; print message hello world
-   mov si, msg
-   call print
+   call load_setup
+   jmp 0x500
 
-   jmp $
-
-
-; Prints a string to screen
-; Params:
-;  - ds:si points to string
-print:
-   push si
-   push ax
-   push bx
-
-   mov ah, 0x0E      ; call bios interrupt, refer to https://zh.wikipedia.org/wiki/INT_10H
-   mov bh, 0         ; set page number to 0
-
-.loop
-   mov al, [si]      ; loads character in al
-   cmp al, 0         ; verify if next character is null
-   jz .done
-   int 0x10
-
-   inc si
-   jmp .loop
-
-.done
-   pop bx
-   pop ax
-   pop si
-   ret
+   ; cli
+   hlt
 
 
-msg: db 'Hello world!', ENDL, 0
+; Read 1 sector from disk
+load_setup:
+    mov ah, 0x02     ; function number:AH=0x02 means read hard disk sector
+    mov al, 0x01     ; 1 sector to read
+    mov ch, 0x00     ; cylinder
+    mov cl, 0x02     ; started sector offset
+    mov dh, 0x00     ; head
+    mov dl, 0x80     ; driver number
+
+    mov bx, 0x500    ; data should be after the bootloader
+
+    int 0x13
+
+    ret
+
+
 
 times 510 - ($ - $$) db 0
 db 0x55, 0xaa
