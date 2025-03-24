938a26f9400d1068c22c0c8f0f6fd884a5322d8e
Qiu <tracyqiu@126.com>
2025-03-24 16:03:55 +0800

    improve print function in stdio.c with supporing of the scroll screen

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -4,6 +4,35 @@
 //__attribute__ ((section ("cstart"))) 
 void cstart(void)
 {
-    puts("Welcome to roqiu's LnOS demo ^_^!");
+    // clear_screen();
+    puts("1Welcome to roqiu's LnOS demo ^_^!");
+    puts("2Welcome to roqiu's LnOS demo ^_^!");
+    puts("3Welcome to roqiu's LnOS demo ^_^!");
+    puts("4Welcome to roqiu's LnOS demo ^_^!");
+    puts("5Welcome to roqiu's LnOS demo ^_^!");
+    // puts("6Welcome to roqiu's LnOS demo ^_^!");
+    // puts("7Welcome to roqiu's LnOS demo ^_^!");
+    // puts("8Welcome to roqiu's LnOS demo ^_^!");
+    // puts("9Welcome to roqiu's LnOS demo ^_^!");
+    // puts("10Welcome to roqiu's LnOS demo ^_^!");
+    // puts("11Welcome to roqiu's LnOS demo ^_^!");
+    // puts("12Welcome to roqiu's LnOS demo ^_^!");
+    // puts("13Welcome to roqiu's LnOS demo ^_^!");
+    // puts("14Welcome to roqiu's LnOS demo ^_^!");
+    // puts("15Welcome to roqiu's LnOS demo ^_^!");
+    // puts("16Welcome to roqiu's LnOS demo ^_^!");
+    // puts("17Welcome to roqiu's LnOS demo ^_^!");
+    // puts("18Welcome to roqiu's LnOS demo ^_^!");
+    // puts("19Welcome to roqiu's LnOS demo ^_^!");
+    // puts("20Welcome to roqiu's LnOS demo ^_^!");
+    // puts("21Welcome to roqiu's LnOS demo ^_^!");
+    // puts("22Welcome to roqiu's LnOS demo ^_^!");
+    // puts("23Welcome to roqiu's LnOS demo ^_^!");
+    // puts("24Welcome to roqiu's LnOS demo ^_^!");
+    // puts("25Welcome to roqiu's LnOS demo ^_^!");
+    // puts("26Welcome to roqiu's LnOS demo ^_^!");
+    // puts("27Welcome to roqiu's LnOS demo ^_^!");
+    // puts("28Welcome to roqiu's LnOS demo ^_^!");
+    
     for (;;);
 }
--- src/kernel/stdio.h
+++ src/kernel/stdio.h
@@ -2,5 +2,6 @@
 #define STDIO_H_
 
 void puts(const char* str);
+void clear_screen();
 
 #endif //STDIO_H_
